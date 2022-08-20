1696f146aaabfb84fa2f502098ea5b8776f83f5a
Danil <danil@vast.com>
2022-08-20 15:07:26 -0400

    Add IO abstractions

--- ohce/v20220820.py
+++ ohce/v20220820.py
@@ -1,8 +1,32 @@
 class V20220820:
-    @staticmethod
-    def no_op():
-        pass
+    class ReadOneLine:
+        def __init__(self, stdin):
+            self.stdin_ = stdin
+
+        def __call__(self):
+            return self.stdin_()
+
+    class FlushLines:
+        def __init__(self, stdout):
+            self.stdout_ = stdout
+
+        def __call__(self, lines):
+            self.stdout_(lines, flush=True)
+
+    class InteractiveLoop:
+        def __init__(self, readOneLine, flushLines):
+            pass
+
+        def __call__(self):
+            pass
+
 
     @staticmethod
     def interactive_loop(*, stdin, stdout, epoch_seconds):
-        return V20220820.no_op
+        read_one_line = V20220820.ReadOneLine(stdin)
+        flush_lines = V20220820.FlushLines(stdout)
+
+        return V20220820.InteractiveLoop(
+            read_one_line,
+            flush_lines
+        )
