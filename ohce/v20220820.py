a7fcff5903036f491dd159c352ade6a0ad4dc20d
Danil <danil@vast.com>
2022-08-20 18:59:59 -0400

    Applying information hiding to I/O

--- ohce/v20220820.py
+++ ohce/v20220820.py
@@ -3,19 +3,19 @@ import time
 
 class V20220820:
     @staticmethod
-    def live(stdin=input, stdout=print, epoch_seconds=time.time):
+    def live():
         # TODO: notice that we haven't yet incorporated the
         #  connection to the clock.
         return V20220820.InteractiveLoop (
-            V20220820.ReadOneLine(
-                stdin
-            ),
-            V20220820.FlushLines(
-                stdout
-            ),
+            V20220820.ReadOneLine.live(),
+            V20220820.FlushLines.live()
         )
 
     class ReadOneLine:
+        @staticmethod
+        def live():
+            return V20220820.ReadOneLine(input)
+
         def __init__(self, stdin):
             self.stdin_ = stdin
 
@@ -23,6 +23,10 @@ class V20220820:
             return self.stdin_()
 
     class FlushLines:
+        @staticmethod
+        def live():
+            return V20220820.FlushLines(print)
+
         def __init__(self, stdout):
             self.stdout_ = stdout
 
