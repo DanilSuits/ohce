1bef67363d7d96cd224995ca6041d05b2383d72f
Danil <danil@vast.com>
2022-08-21 08:56:26 -0400

    Sketching out the interactive loop

--- doc/notes/checklist.md
+++ doc/notes/checklist.md
@@ -1,2 +1,6 @@
-Do we need to replace the integer representation of epoch time
+- Do we need to replace the integer representation of epoch time
 with an abstraction that hides the underlying data structure?
+  
+- Can we refactor the interactive loop so that it is clear where
+in the sequence we are blocking for user input, and which side
+effects are deliberately executed subsequent to that input?
--- ohce/v20220820.py
+++ ohce/v20220820.py
@@ -6,11 +6,22 @@ class V20220820:
     def live():
         # TODO: notice that we haven't yet incorporated the
         #  connection to the clock.
-        return V20220820.InteractiveLoop (
+        return V20220820.InteractiveLoop.build (
             V20220820.ReadOneLine.live(),
             V20220820.FlushLines.live(),
             V20220820.EpochSeconds.live(),
+            V20220820.FiniteStateMachine()
         )
+
+    class FiniteStateMachine:
+        def running(self):
+            return False
+
+        def on_input(self, line, now):
+            pass
+
+        def output(self):
+            return None
 
     class EpochSeconds:
         @staticmethod
@@ -59,9 +70,32 @@ class V20220820:
             return locals()
 
     class InteractiveLoop:
-        def __init__(self, readOneLine, flushLines, epoch_seconds):
-            pass
+        @staticmethod
+        def build(read_one_line, flush_lines, epoch_seconds, fsm):
+            return V20220820.InteractiveLoop(
+                read_one_line,
+                flush_lines,
+                epoch_seconds,
+                fsm.running,
+                fsm.on_input,
+                fsm.output
+            )
+
+        def __init__(self, read_one_line, flush_lines, epoch_seconds, running, on_input, output):
+            self.read_one_line = read_one_line
+            self.flush_lines = flush_lines
+            self.epoch_seconds = epoch_seconds
+            self.running = running
+            self.on_input = on_input
+            self.output = output
 
         def __call__(self):
-            pass
+            while self.running():
+                line = self.read_one_line()
+                now = self.epoch_seconds()
+
+                self.on_input(line, now)
+
+                output = self.output()
+                self.flush_lines(output)
 
