613773ad423eb57728ff0643822de0308b39ee7c
Danil <danil@vast.com>
2022-08-21 10:25:55 -0400

    Introduce an adapter to separate time effects from I/O effects

--- ohce/v20220820.py
+++ ohce/v20220820.py
@@ -70,21 +70,34 @@ class V20220820:
             return locals()
 
     class InteractiveLoop:
+        class Adapter:
+            def __init__(self, epoch_seconds, on_input):
+                self.epoch_seconds = epoch_seconds
+                self.core = on_input
+
+            def on_input(self, line):
+                now = self.epoch_seconds()
+                self.core(line, now)
+
+
         @staticmethod
         def build(read_one_line, flush_lines, epoch_seconds, fsm):
+            adapter = V20220820.InteractiveLoop.Adapter(
+                epoch_seconds,
+                fsm.on_input,
+            )
+
             return V20220820.InteractiveLoop(
                 read_one_line,
                 flush_lines,
-                epoch_seconds,
                 fsm.running,
-                fsm.on_input,
+                adapter.on_input,
                 fsm.output
             )
 
-        def __init__(self, read_one_line, flush_lines, epoch_seconds, running, on_input, output):
+        def __init__(self, read_one_line, flush_lines, running, on_input, output):
             self.read_one_line = read_one_line
             self.flush_lines = flush_lines
-            self.epoch_seconds = epoch_seconds
             self.running = running
             self.on_input = on_input
             self.output = output
@@ -92,9 +105,8 @@ class V20220820:
         def __call__(self):
             while self.running():
                 line = self.read_one_line()
-                now = self.epoch_seconds()
 
-                self.on_input(line, now)
+                self.on_input(line)
 
                 output = self.output()
                 self.flush_lines(output)
